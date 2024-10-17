93a6b665064fce4e9edc539abb8dbb682fc4770c
praveenmahesh06 <praveenmahesh06@gmail.com>
2024-10-17 14:17:29 +0530

    Add examples of Operators

---
+++ basics/Operators/Comparision.java
@@ -0,0 +1,16 @@
+package basics.Operators;
+
+public class Comparision {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    System.out.println("State True/False");
+    System.out.println("-".repeat(30));
+    System.out.println("Whether 10 and 20 are equal: " + (a == b));// Return false since 10 and 20 are not equal
+    System.out.println("10 and 20 are not equal: " + (a != b));// returns True
+    System.out.println("10 is lesser than 20 : " + (a < b));// returns True
+    System.out.println("30 is greater than 20: " + (c > b));// returns True
+  }
+}
+// javac basics/Operators/Comparision.java && java basics.Operators.Comparision
