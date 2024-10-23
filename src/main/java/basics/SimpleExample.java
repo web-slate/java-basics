770a7f7e7c9b6fdbb893776a927696667cdbdc91
Venkat.R <ramsunvtech@gmail.com>
2024-10-23 08:07:37 +0800

    tech: base changes

--- src/main/java/basics/SimpleExample.java
+++ src/main/java/basics/SimpleExample.java
@@ -1,13 +1,10 @@
 package basics;
 
-public class Basics {
+public class SimpleExample {
   public static void main(String[] args) {
     // Below accept parameter and print along with "Java Basics".
     System.out.println("Java Basics " + args[0]); // Here args[0] meant that its first word.
   }
 }
 
-/**
- * Execute below command.
-    javac basics/Basics.java && java basics.Basics hello
- */
+// javac basics/Basics.java && java basics.Basics hello
--- src/main/java/leet_code/palindrome/QuickSolution.java
+++ src/main/java/leet_code/palindrome/QuickSolution.java
@@ -1,12 +1,20 @@
 package leet_code.palindrome;
 
 public class QuickSolution {
+  public static void main(String[] args) {
+    QuickSolution solution = new QuickSolution();
+    boolean result = solution.isPalindrome("madam");
+    System.out.println("Is 'madam' palindrome: " + result);
+  }
+
   public boolean isPalindrome(String s) {
-      String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-      return cleaned.equals(new StringBuilder(cleaned).reverse().toString());
+    String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    return cleaned.equals(new StringBuilder(cleaned).reverse().toString());
   }
 }
 
-// javac leet_code/palindrome/QuickSolution.java && java leet_code.palindrome.QuickSolution
-// mvn -Dtest=leet_code.palindrome.tests.QuickSolutionTest test
-// mvn -Dtest=QuickSolutionTest test
+/**
+ * javac leet_code/palindrome/QuickSolution.java
+ * java leet_code.palindrome.QuickSolution
+ */
+
